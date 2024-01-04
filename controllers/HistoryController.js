34a3ef8c9a30210922b83536376f51c58601a1de
gentaarya <mgentaarya@gmail.com>
2024-01-04 11:11:07 +0700

    migrate database

--- controllers/HistoryController.js
+++ controllers/HistoryController.js
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 export const getHistory = async (req, res) => {
   const { username } = req.params;
+  const { page = 1, perPage = 5 } = req.query;
+
+  const skip = (page - 1) * perPage;
 
   try {
     const orders = await prisma.order.findMany({
@@ -14,9 +17,23 @@ export const getHistory = async (req, res) => {
         orderDetails: true,
         location: true,
       },
+      skip: skip,
+      take: parseInt(perPage),
     });
 
-    res.json(orders);
+    const totalOrders = await prisma.order.count({
+      where: {
+        username: username,
+      },
+    });
+
+    const totalPages = Math.ceil(totalOrders / perPage);
+
+    res.json({
+      orders,
+      currentPage: parseInt(page),
+      totalPages,
+    });
   } catch (error) {
     console.error("Error retrieving orders:", error);
     res.status(500).send("Internal Server Error");
